68eaccfdd2b13bdac951271bc4bb5a66adfdb3c3
LUALROAL <alejorodryguez@gmail.com>
2025-08-14 20:51:43 -0500

    feat: update dependencies and add Angular Material with custom theming
    - Added Angular CDK and Material dependencies to package.json.
    - Removed placeholder content from app.component.html and replaced with a router outlet.
    - Updated app.config.ts to include provideAnimationsAsync for better animation handling.
    - Modified index.html to include Google Fonts and Material Icons, applying mat-typography class to body.
    - Created custom theming for Angular Material in styles.scss, defining a light theme with primary and tertiary colors.
    - Initialized tailwind.config.js for future Tailwind CSS integration.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { JwtInterceptor } from './core/auth/interceptors/jwt.interceptor'; // Ajusta la ruta según tu carpeta
+import { JwtInterceptor } from './core/auth/interceptors/jwt.interceptor';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; // Ajusta la ruta según tu carpeta
 
 
 export const appConfig: ApplicationConfig = {
@@ -12,7 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(
       withInterceptors([JwtInterceptor])
-    )
+    ), provideAnimationsAsync()
   ]
   // providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
 };
