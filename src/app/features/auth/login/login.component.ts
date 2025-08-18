b62f2c2bf2516f524fe0e58b35fd642837854855
LUALROAL <alejorodryguez@gmail.com>
2025-08-18 14:46:01 -0500

    Ajustes login y registro

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -144,24 +144,48 @@ export class LoginComponent {
   }
   ngOnInit() {
 
-    google.accounts.id.initialize({
-      client_id: '1039061657987-tqdli0gkper6a2rtq7sd1i73pmelqn0c.apps.googleusercontent.com',
-      callback: (response: any) => this.handleGoogleLogin(response.credential),
-    });
+//     google.accounts.id.initialize({
+//       client_id: '1039061657987-tqdli0gkper6a2rtq7sd1i73pmelqn0c.apps.googleusercontent.com',
+//       callback: (response: any) => this.handleGoogleLogin(response.credential),
+//     });
 
-  google.accounts.id.renderButton(
-  document.getElementById("googleBtn"),
-  {
-      theme: "outline",   // outline | filled_blue | filled_black
-    size: "large",      // small | medium | large
-    shape: "pill",      // rect | pill | circle | square
-    text: "continue_with", // o "signin_with"
-    logo_alignment: "left", // o "center"
-    width: 380    // ancho en px
-  }
-);
+//   google.accounts.id.renderButton(
+//   document.getElementById("googleBtn"),
+//   {
+//       theme: "outline",   // outline | filled_blue | filled_black
+//     size: "large",      // small | medium | large
+//     shape: "pill",      // rect | pill | circle | square
+//     text: "continue_with", // o "signin_with"
+//     logo_alignment: "left", // o "center"
+//     width: 380    // ancho en px
+//   }
+// );
 
   }
+
+  ngAfterViewInit() {
+    if (typeof google !== 'undefined') {
+      google.accounts.id.initialize({
+        client_id: '1039061657987-tqdli0gkper6a2rtq7sd1i73pmelqn0c.apps.googleusercontent.com',
+        callback: (response: any) => this.handleGoogleLogin(response.credential),
+      });
+
+      google.accounts.id.renderButton(
+        document.getElementById("googleBtn"),
+        {
+          theme: "outline",
+          size: "large",
+          shape: "pill",
+          text: "continue_with",
+          logo_alignment: "left",
+          width: 380
+        }
+      );
+    } else {
+      console.error("⚠️ Google script no cargado aún.");
+    }
+  }
+
 onSubmit() {
   if (this.loginForm.invalid) return;
 
