9642b472315493cfd84c18eb1ed9cf13cb44f356
LUALROAL <alejorodryguez@gmail.com>
2025-08-26 15:29:41 -0500

    ajuste rutas

--- src/app/features/admin/admin.routes.ts
+++ src/app/features/admin/admin.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { AdminLayoutComponent } from '../../layouts/admin-layout/admin-layout.component';
 import { AdminGuard } from '../../core/auth/guards/adminGuard';
 import { AuthGuard } from '../../core/auth/guards/auth.guard';
+import { ProductsListComponent } from '../products/components/products-list/products-list.component';
+import { ProductFormComponent } from '../products/components/product-form/product-form.component';
+import { ProductDetailComponent } from '../products/components/product-detail/product-detail.component';
 
 export const ADMIN_ROUTES: Routes = [
   {
@@ -12,39 +15,33 @@ export const ADMIN_ROUTES: Routes = [
       {
         path: 'admin-dashboard',
         loadComponent: () => import('./components/admin-dashboard/admin-dashboard.component').then(m => m.AdminDashboardComponent),
-        canLoad: [AuthGuard, AdminGuard]
       },
       {
         path: 'perfil',
         loadComponent: () => import('./components/admin-profile/admin-profile.component').then(m => m.AdminProfileComponent),
-        canLoad: [AuthGuard, AdminGuard]
       },
       {
         path: 'productos',
-        loadComponent: () => import('../products/components/products-list/products-list.component').then(m => m.ProductsListComponent),
-        canLoad: [AuthGuard, AdminGuard]
+        children: [
+            {
+                path: '',
+                component: ProductsListComponent,
+                pathMatch: 'full'
+            },
+            {
+                path: 'new',
+                component: ProductFormComponent
+            },
+            {
+                path: 'edit/:id',
+                component: ProductFormComponent
+            },
+            {
+                path: 'detalles/:id',
+                component: ProductDetailComponent
+            }
+        ]
       }
-      //   path: 'productos',
-      //   // loadComponent: () => import('./admin-products/admin-products.component').then(m => m.AdminProductsComponent)
-      // },
-
-      // {
-      //   path: 'usuarios',
-      //   // loadComponent: () => import('./admin-users/admin-users.component').then(m => m.AdminUsersComponent)
-      // },
-
-
-
-      // {
-      //   path: 'empresas',
-      //   // loadComponent: () => import('./admin-companies/admin-companies.component').then(m => m.AdminCompaniesComponent)
-      // },
-
-      // {
-      //   path: 'escaneos',
-      //   // loadComponent: () => import('./admin-scans/admin-scans.component').then(m => m.AdminScansComponent)
-      // }
-
     ]
   }
 ];
